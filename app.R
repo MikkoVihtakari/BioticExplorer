ca9542b8cb47a609385ceab719d09a199a074c03
Vihtakari <a22357@HI-11155.hi.no>
2019-09-24 13:14:51 +0200

    Version change (forgot during last commit)

--- app.R
+++ app.R
@@ -58,7 +58,7 @@ header <- dashboardHeader(title = div(
     column(width = 10, p("Biotic Explorer", align = "center"))
   )
 ),
-dropdownMenu(type = "notifications", headerText = "Version 0.2.5 (alpha), 2019-09-15",
+dropdownMenu(type = "notifications", headerText = "Version 0.2.6 (alpha), 2019-09-24",
              icon = icon("cog"), badgeStatus = NULL,
              notificationItem("Download NMD data", icon = icon("download"), status = "info", href = "https://datasetexplorer.hi.no/"),
              notificationItem("Explanation of data types and codes", icon = icon("question-circle"), status = "info", href = "https://hinnsiden.no/tema/forskning/PublishingImages/Sider/SPD-gruppen/H%C3%A5ndbok%205.0%20juli%202019.pdf#search=h%C3%A5ndbok%20pr%C3%B8vetaking"),
