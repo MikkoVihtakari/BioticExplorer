7d382bde65e869d0225854147ecc73507dc2d2ec
Vihtakari <a22357@HI-11155.hi.no>
2020-01-22 16:02:12 +0100

    Preparing for beta-release

--- app.R
+++ app.R
@@ -59,7 +59,7 @@ header <- dashboardHeader(title = div(
     column(width = 10, p("Biotic Explorer", align = "center"))
   )
 ),
-dropdownMenu(type = "notifications", headerText = "Version 0.3.0 (alpha), 2020-01-23",
+dropdownMenu(type = "notifications", headerText = "Version 0.3.0 (alpha), 2020-01-22",
              icon = icon("cog"), badgeStatus = NULL,
              notificationItem("Download NMD data", icon = icon("download"), status = "info", href = "https://datasetexplorer.hi.no/"),
              notificationItem("Explanation of data types and codes", icon = icon("question-circle"), status = "info", href = "https://hinnsiden.no/tema/forskning/PublishingImages/Sider/SPD-gruppen/H%C3%A5ndbok%205.0%20juli%202019.pdf#search=h%C3%A5ndbok%20pr%C3%B8vetaking"),
