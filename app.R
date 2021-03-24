2a5ce8bec4b26952b84a7c856708a841a3defaa1
Ibrahim Umar <ibrahim.umar@hi.no>
2021-03-25 00:31:01 +0100

    Separate package requirements

--- app.R
+++ app.R
@@ -20,32 +20,8 @@ if(os == "Linux") {
   }
 }
 
-### Install missing packages
-
-required.packages <- c("shiny" = TRUE, "shinyFiles" = TRUE, "shinydashboard" = TRUE, "DT" = TRUE, 
-                       "data.table" = TRUE,  "tidyverse" = TRUE, "dtplyr" = TRUE, "devtools" = FALSE,
-                       "leaflet" = TRUE, "leaflet.minicharts" = TRUE, "plotly" = TRUE, 
-                       "openxlsx" = FALSE, "scales" = FALSE, "fishmethods" = FALSE, "viridis" = FALSE,
-                       "mapview" = FALSE, "DBI" = FALSE, "bsplus" = TRUE) ## TRUE means that the package should be loaded. FALSE that the functions are used without loading the package (by refering ::)
-
-new.packages <- names(required.packages)[!(names(required.packages) %in% installed.packages()[,"Package"])]
-if (length(new.packages) > 0) install.packages(new.packages)
-
-sapply(names(required.packages)[required.packages], require, character.only = TRUE)
-
-#### RstoxData
-# (not loaded, use pointer when using the functions)
-
-if (!"RstoxData" %in% installed.packages()[,"Package"]) {
-  install.packages("RstoxData", repo="https://stoxproject.github.io/repo")
-}
-
-#### DuckDB
-# (not loaded, use pointer when using the functions)
-
-if (!"duckdb" %in% installed.packages()[,"Package"]) {
-  install.packages("duckdb")
-}
+## Install pre-requisites
+source("install_requirements.R")
 
 ## Source functions used by the app
 
